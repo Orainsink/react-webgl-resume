d0856b169df24cadb95e911667ace535cd4314d3
Orainsink <ywt1250066597@gmail.com>
2019-11-28 15:38:42 +0800

    Update: 添加loader页面,及preload images

--- src/index.js
+++ src/index.js
@@ -8,15 +8,36 @@ import { makeStore } from "./js/store";
 
 import * as serviceWorker from "./serviceWorker";
 
-import "./js/libs/preloadImages";
+import {imgList}from "./js/libs/preloadImages";
+import ImagesLoader from "./js/libs/imagesLoader";
 
-const store = makeStore();
+let loader = document.getElementById("loader");
+const imagesLoader = new ImagesLoader(imgList);
+let text = document.getElementsByClassName("loader__text")[0];
+let bar = document.getElementsByClassName("loader__bar")[0];
+imagesLoader.start();
+imagesLoader.onProgress(percent => {
+  text.innerText = `${Math.round(percent)}%`;
+  bar.style.width = `${Math.round(percent)}%`;
+});
+imagesLoader.onComplete(() => {
+  text.innerText = "100%";
+  bar.style.width = "100%";
+  setTimeout(() => {
+    loader.style.display = "none";
+    startApp()
+  }, 2000);
+});
 
-ReactDOM.render(
-	<StoreContext.Provider value={store}>
-		<App />
-	</StoreContext.Provider>,
-	document.getElementById("root")
-);
+function startApp(){
+  const store = makeStore();
+
+  ReactDOM.render(
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>,
+    document.getElementById("root")
+  );
+}
 
 serviceWorker.unregister();
--- src/js/libs/preloadImages.js
+++ src/js/libs/preloadImages.js
@@ -1,21 +1,32 @@
 // 预加载图片, 加载完成过后结束loading, 因为考虑到bundle文件较大加载慢,影响用户体验, 所以直接控制真实DOM.
-import ImagesLoader from "../libs/imagesLoader";
-
-(function() {
-	let loader = document.getElementById("loader");
-	const imagesLoader = new ImagesLoader([]);
-	let text = document.getElementsByClassName("loader__text")[0];
-	let bar = document.getElementsByClassName("loader__bar")[0];
-	imagesLoader.start();
-	imagesLoader.onProgress(percent => {
-		text.innerText = `${Math.round(percent)}%`;
-		bar.style.width = `${Math.round(percent)}%`;
-	});
-	imagesLoader.onComplete(() => {
-		text.innerText = "100%";
-		bar.style.width = "100%";
-		setTimeout(() => {
-			loader.style.display = "none";
-		}, 2000);
-	});
-})();
+export const imgList = [
+	"../../assets/images/heightMap-A.jpg",
+	"../../assets/images/heightMap-B.jpg",
+	"../../assets/images/heightMap-O.jpg",
+	"../../assets/images/matCap.jpg",
+	"../../assets/images/matCap-shiny.jpg",
+	"../../assets/images/part-beam.png",
+	"../../assets/images/part-drop.png",
+	"../../assets/images/part-face.png",
+	"../../assets/images/part-field.png",
+	"../../assets/images/part-galaxy.png",
+	"../../assets/images/part-gravity.png",
+	"../../assets/images/part-height.png",
+	"../../assets/images/part-grid.png",
+	"../../assets/images/part-neons.png",
+	"../../assets/images/part-rocks.png",
+	"../../assets/images/part-smoke.png",
+	"../../assets/images/part-sphere.png",
+	"../../assets/images/part-stars.png",
+	"../../assets/images/sprite-none.png",
+	"../../assets/images/sprite-smoke.png",
+	"../../assets/images/texture-ball.png",
+	"../../assets/images/texture-ballAlpha.png",
+	"../../assets/images/texture-drop.png",
+	"../../assets/images/texture-laserBody.png",
+	"../../assets/images/texture-laserCap.png",
+	"../../assets/images/texture-laserFlare.png",
+	"../../assets/images/texture-neonGlow.png",
+	"../../assets/images/texture-neonProjection.png",
+	"../../assets/images/texture-wave.png"
+];
